e355496f8bb8513c533f725b637a10dfcb20c7f1
BuddyBoyVilla <74051918+BuddyNotFound@users.noreply.github.com>
2023-07-09 20:36:48 +0300

    Update deleteme.lua

--- client/deleteme.lua
+++ client/deleteme.lua
@@ -1,9 +1,1 @@
-RegisterCommand("test1",function()
-    SetEntityCoords(PlayerPedId(),Config.Targets.GetPlane.pos)
-end)
 
-RegisterCommand('test2',function(source)
-    local src = source
-    print(Config.DeliveryLocations[1].busy[src])
-    print(Config.DeliveryLocations[2].busy[src])
-end)
